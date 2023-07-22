7b114e77014c0f09ea19b03cb4d6c9c4bc39621d
qsu <51903703+ykkaq@users.noreply.github.com>
2023-07-22 21:50:31 +0900

    kari

--- src/sender.ts
+++ src/sender.ts
@@ -1,9 +1,18 @@
-function sender(){
+function doSend(){
   write_sheet();
 }
 
 // discord -> gas
 
+interface remind{
+  id: number; 
+  date: Date;
+  description: string;
+  discord_channel: number;
+  from: string;
+}
+
+/*
 class remind{
   constructor() {
     this.id = -1;
@@ -13,11 +22,27 @@ class remind{
     this.from = '6mink';
   }
 }
+*/
 
 function write_sheet() {
   console.log("run");
 
-  let test = new remind();
+  let test = {
+    id: -1,
+    date: new Date('2024-01-01T00:00+09:00'),
+    description: 'test',
+    discord_channel: 537871940581326868,
+    _from: 'tester'
+  };
+
+  /*
+  let test = {} as remind;
+  test.id = -1;
+  test.date = new Date('2024-01-01T00:00+09:00');
+  test.description = 'test';
+  test.discord_channel = 537871940581326868;
+  test.from = 'tester';
+  */
 
   // spreadsheetの取得
   let sheet_file = SpreadsheetApp.getActiveSpreadsheet();
@@ -29,12 +54,9 @@ function write_sheet() {
   let row_len = 5; 
   let cell_alphabet = new Array(row_len);
 
-  for (v in cell_alphabet) {
-    
+  for (let i = 0; i < row_len;i++) {
+    cell_alphabet[i] = 'A' + i;
   }
 
-
-  let atId = sheet_table.getRange('A3');
-  atId.setValue(test.id);
-  
+  console.log(cell_alphabet);
 }
